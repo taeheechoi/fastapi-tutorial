47a14c0d3ddd6a7c914935e9a1b9bb7aaf76dd63
Tae Hee Choi <dadac76@hotmail.com>
2021-11-09 22:01:37 -0800

    fontend

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Layout } from "../../components"
+import { EuiText } from "@elastic/eui"
+export default function App() {
+  return (
+    <Layout>
+      <EuiText>
+        <h1>Phresh</h1>
+      </EuiText>
+    </Layout>
+  )
+}
---
+++ frontend/src/components/index.js
@@ -0,0 +1,2 @@
+export { default as App } from "./App/App"
+export { default as Layout } from "./Layout/Layout"
